be701278ff44ea177a297e537e1ee1323c0b149f
salikbubu <salikbula@gmail.com>
2023-06-19 12:23:05 +0200

    tailwind installed

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto' , "sans-serif"],
+      },
+      colors: {
+        blue: "#669999",
+        darkblue: "#336666",
+        light: "#fdfdfd",
+        drak: "#333",
+      },
+    },
+  },
+  plugins: [],
+};
